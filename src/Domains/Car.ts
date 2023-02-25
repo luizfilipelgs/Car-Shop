c88561bf4dba2e576c20b410b1934663d3dd39ed
luizfilipelgs <luizfilipelgs@gmail.com>
2023-02-25 18:57:57 -0300

    feat: class car

---
+++ src/Domains/Car.ts
@@ -0,0 +1,17 @@
+import ICar from '../Interfaces/Icar';
+import Vehicle from './Vehicle';
+
+export default class Car extends Vehicle {
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(car: ICar) {
+    super(car);
+    this.doorsQty = car.doorsQty;
+    this.seatsQty = car.seatsQty;
+  }
+
+  public getDoorsQty(): number { return this.doorsQty; }
+  
+  public getSeatsQty(): number { return this.seatsQty; }
+}
