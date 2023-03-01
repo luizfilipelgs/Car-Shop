d8edc60a9c48a5aedaa2d64b6091ca4db95adc4e
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-01 02:07:00 -0300

    feat: controllers de cars

--- src/Controllers/Cars.controller.ts
+++ src/Controllers/Cars.controller.ts
@@ -24,6 +24,43 @@ class CarController {
       this.next(error);
     }
   }
+
+  public async findAll() {
+    try {
+      const { status, result } = await this.service.getAll();
+      return this.res.status(status).json(result);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async findById() {
+    const { id } = this.req.params;
+
+    try {
+      const { message, status, result } = await this.service.findById(id);
+        
+      if (message) return this.res.status(status).json({ message });
+
+      return this.res.status(status).json(result);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async update() {
+    const { id } = this.req.params;
+    const { body } = this.req;
+
+    try {
+      const { message, status, result } = await this.service.update(id, body);
+
+      if (message) return this.res.status(status).json({ message });
+      return this.res.status(status).json(result);
+    } catch (error) {
+      return this.next(error);
+    }
+  }
 }
 
 export default CarController;
