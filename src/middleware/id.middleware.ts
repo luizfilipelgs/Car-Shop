ceb18f2967fb70587c49b0f64c3b65f47a836c05
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-01 02:07:23 -0300

    feat: middlewares de erro e id

---
+++ src/middleware/id.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import { isValidObjectId } from 'mongoose';
+
+const verifyId = (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) return res.status(422).json({ message: 'Invalid mongo id' });
+  
+  return next();
+};
+
+export default verifyId;
