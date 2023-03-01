e1a71d477a0553274cb1f62ba320a75d276444a0
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-01 13:22:06 -0300

    test-mock: mocks de car

---
+++ tests/unit/Services/car.mock.ts
@@ -0,0 +1,61 @@
+const carModelMock = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const carMock = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const carUpdateMock = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Red',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const listCarMock = [
+  {
+    id: '6348513f34c397abcad040b1',
+    model: 'Jeep',
+    year: 2023,
+    color: 'Black',
+    status: true,
+    buyValue: 20.000,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
+export {
+  carModelMock,
+  carMock,
+  listCarMock,
+  carUpdateMock,
+};
