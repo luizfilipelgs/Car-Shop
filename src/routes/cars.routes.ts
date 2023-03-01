025fc4408f274549fa493ddd97e15c0e1affc7ed
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-01 02:08:40 -0300

    feat: get e put  /cars e /cars/:id

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import CarController from '../Controllers/Cars.controller';
+import verifyId from '../middleware/id.middleware';
 
 const routes = Router();
 
 routes.post('/', (req, res, next) => new CarController(req, res, next).create());
+routes.get('/', (req, res, next) => new CarController(req, res, next).findAll());
+routes.get('/:id', verifyId, (req, res, next) => new CarController(req, res, next).findById());
+routes.put('/:id', verifyId, (req, res, next) => new CarController(req, res, next).update());
 
 export default routes;
