98ba0fd1d8c4f383e4e9d292936cc8e88cfbb773
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-01 13:22:24 -0300

    test-mock: motorcycle

---
+++ tests/unit/Services/motorcycle.mock.ts
@@ -0,0 +1,61 @@
+const motoModelMock = {
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+const motoMock = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const motoUpdateMock = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Red',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const listMotoMock = [
+  {
+    id: '6348513f34c397abcad040b1',
+    model: 'Jeep',
+    year: 2023,
+    color: 'Black',
+    status: true,
+    buyValue: 20.000,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
+export {
+  motoModelMock,
+  motoMock,
+  listMotoMock,
+  motoUpdateMock,
+};
